5c3249d8a68323a08d1ae480208e91d2e567ff83
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-11-03 14:05:31 -0600

    complete

--- app.ts
+++ app.ts
@@ -1,40 +1,46 @@
-var myName = 'Amanda';
-var usStates = 50;
-var sum = 4 + 5;
+const myName: string = 'Amanda';
+const usStates: number = 50;
+const sum: number = 4 + 5;
+
+
+
 // Hello World alert
 function sayHello() {
     alert('Hello World');
-}
-;
+};
 sayHello();
+
 // checking age
-function checkAge(name, age) {
+function checkAge(name: string, age: number) {
     if (age < 21) {
-        alert("Sorry " + name + ", you are not old enough to view this page!");
-    }
-    ;
-}
-;
+        alert("Sorry " + name + ", you are not old enough to view this page!")
+    };
+};
+
 checkAge("Charles", 21);
 checkAge("Abby", 27);
 checkAge("James", 18);
 checkAge("John", 17);
+
 // favorite veggies
-var favVeggies = ["Asparagus", "Brussel Sprouts", "Sweet Potatoes"];
+let favVeggies = ["Asparagus", "Brussel Sprouts", "Sweet Potatoes"];
+
 // loop for veggies
-for (var i = 0; i < favVeggies.length; ++i) {
+for (let i = 0; i < favVeggies.length; ++i) {
     console.log(favVeggies[i]);
-}
-;
+};
+
 //object for pet
-var pet = {
+let pet = {
     name: 'Molly',
     breed: 'Calico'
 };
+
 // console log for pet
 console.log(pet.name, pet.breed);
+
 // 5 objects with name and age
-var people = [
+let people = [
     {
         name: "Amy",
         age: 32
@@ -56,23 +62,26 @@ var people = [
         age: 18
     },
 ];
+
 // run objects through loop
-for (var i = 0; i < people.length; i++) {
+for (let i = 0; i < people.length; i++) {
     checkAge(people[i].name, people[i].age);
 }
+
 // getLength function
-function getLength(word) {
+function getLength(word: string) {
     return word.length;
 }
-var wordLength = getLength('Hello World');
+
+let wordLength: number = getLength('Hello World');
 console.log(wordLength);
-function isEven(length) {
+
+function isEven(length: any) {
     if (length % 2 == 0) {
         console.log('The world is nice and even!');
-    }
-    else {
+    } else {
         console.log('The world is an odd place!');
     }
-}
-;
+};
+
 isEven(wordLength);
